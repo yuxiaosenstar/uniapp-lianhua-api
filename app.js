e08e6ea3673b64df861ad5470ad68eff1e4d31fd
yuchangchang <yuchangchang@hd123.com>
2021-04-29 17:57:44 +0800

    - https

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const bodyParser = require('koa-bodyparser')
 const controller = require('./controller')
 const cors = require('koa2-cors')
 
+// https相关
+// const config = require('./config/config');
+// const https = require('https');
+// const fs = require('fs');
+// const enforceHttps = require('koa-sslify').default;
+
 const app = new Koa()
 
 // log request URL:
@@ -18,5 +24,15 @@ app.use(bodyParser())
 // add controllers:
 app.use(controller())
 
+// Force HTTPS on all page
+// app.use(enforceHttps());
+// const options = {
+//   key: fs.readFileSync('./ssl/private_key.pem'),
+//   cert: fs.readFileSync('./ssl/ca-cert.pem')
+// };
+// https.createServer(options, app.callback()).listen(3001, () => {
+// });
+
 app.listen(3000)
+
 console.log('app started at port 3000...')
